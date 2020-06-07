e479c803df632d03465894dfc7a7ae1700bffcb6
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-06-07 16:57:23 +0300

    First commit

---
+++ 03-crudapp-jpa-spring-data/src/main/java/com/predoana/springboot/crudapp/service/EmployeeServiceImpl.java
@@ -0,0 +1,49 @@
+package com.predoana.springboot.crudapp.service;
+
+import com.predoana.springboot.crudapp.dao.EmployeeDAO;
+import com.predoana.springboot.crudapp.entity.Employee;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	private EmployeeDAO employeeDAO;
+	
+	public EmployeeServiceImpl(@Qualifier("employeeDAOJpaImpl") EmployeeDAO theEmployeeDAO) {
+		employeeDAO = theEmployeeDAO;
+	}
+	
+	@Override
+	@Transactional
+	public List<Employee> findAll() {
+		
+		return employeeDAO.findAll();
+	}
+
+	@Override
+	@Transactional
+	public Employee findById(int theId) {
+		
+		return employeeDAO.findById(theId);
+	}
+
+	@Override
+	@Transactional
+	public void save(Employee theEmployee) {
+		
+		employeeDAO.save(theEmployee);
+		
+	}
+
+	@Override
+	@Transactional
+	public void deleteById(int theId) {
+
+		employeeDAO.deleteById(theId);
+	}
+
+}
